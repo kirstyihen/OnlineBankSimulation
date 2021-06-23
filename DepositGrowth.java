0e4942e35b0bc911d17397c84c30f9d5f0f1645b
kirstyihen <86343211+kirstyihen@users.noreply.github.com>
2021-06-23 18:02:07 -0400

    Update DepositGrowth.java

--- DepositGrowth.java
+++ DepositGrowth.java
@@ -5,14 +5,16 @@ public class DepositGrowth{
       
       double interestRate= 0.005;
       Scanner input= new Scanner(System.in);
-      System.out.println("Welcome to FinTech Bank valued customer! Enter balance:
-                         ");
+      System.out.println("Welcome to FinTech Bank valued customer! Enter balance:");
       int balance= input.nextInt();
-      double firstMonth= (int)balance + (int)balance * interestRate;
-      double secondMonth= firstMonth + firstMonth * interestRate;
-      double thirdMonth= secondMonth+ secondMonth * interestRate;
+      double firstMonth= (int)balance+((int)balance*interestRate);
+      double secondMonth= firstMonth+(firstMonth*interestRate);
+      double thirdMonth= secondMonth+(secondMonth*interestRate);
       System.out.println("Annual interest rate="+interestRate*100+"%");
       System.out.println("After first month:"+firstMonth);
       System.out.println("After second month:"+secondMonth);
       System.out.println("After third month:"+thirdMonth);
-                       
+                         
+   }
+}
+            
